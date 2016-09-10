3a97e25d5c166abaaeaba9cc100a8398a25d4d11
Rajkumars1971 <rajkumars1971@gmail.com>
2016-09-10 17:02:45 +0530

    Initial version
    First version created for testing purpose of eclipse and Java installation, add hello git message.

---
+++ HelloWorld.java
@@ -0,0 +1,14 @@
+package learning;
+
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hellow GIT ");
+	}
+
+}
+/*http://marketplace.eclipse.org/marketplace-client-intro?mpc_install=114*/
