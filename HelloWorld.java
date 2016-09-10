d9965e5d7d68cbc3427bd6a2e5e31b45b5b6da03
Rajkumars1971 <rajkumars1971@gmail.com>
2016-09-10 17:30:52 +0530

    Add a method to print the message
    Added a new message PrintMessage

--- HelloWorld.java
+++ HelloWorld.java
@@ -5,10 +5,17 @@ public class HelloWorld {
 	/**
 	 * @param args
 	 */
+	 
+	 /* 09/10 Rajkumra : * added a new method Print message, removed static print method from the main method */
+	 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hellow GIT ");
+		 PrintMessage("Hellow Git Welcome back");
+	}
+	
+	private static void PrintMessage(String msg)
+	{
+		System.out.println(msg);
 	}
 
 }
-/*http://marketplace.eclipse.org/marketplace-client-intro?mpc_install=114*/
